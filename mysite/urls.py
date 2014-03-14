c8db01e89e6e3f60e2f7ae50226910b00d21775b
root <root@mamun-VPCEB2AFD.(none)>
2014-03-13 23:51:08 -0700

    Adding hours_ahead function

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import current_datetime
+from mysite.views import current_datetime, hours_ahead
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 import datetime
 
+
 def current_datetime(request):
-    now=datetime.datetime.now()
-    html = "<html><body> It is now %s. </body></html>" %now
+    dt = datetime.datetime.now()
+    html = "<html><body> It is %s now. </body></html>" %(dt)
+    return HttpResponse(html)
+
+
+
+def hours_ahead(request, offset):
+    offset = int(offset)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body> After %s hour(s),<br/> it will be %s. </body></html>" %(offset, dt)
     return HttpResponse(html)
